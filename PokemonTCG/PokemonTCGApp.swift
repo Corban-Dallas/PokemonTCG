f821603cd2a1caeb3bdac25b80f1ca39851e2d2e
Grigory Krivyakin <g.krivyakin.dev@gmail.com>
2021-12-27 16:23:15 +0700

    Initial Commit

---
+++ PokemonTCG/PokemonTCGApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGApp.swift
+//  PokemonTCG
+//
+//  Created by Григорий Кривякин on 27.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTCGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
